a5f40b8a833388af26f9f83c6899249a475e26aa
Phil-Sparkes <phil.sparkes95@gmail.com>
2016-10-11 14:21:03 +0100

    The screen

---
+++ BallMovement.py
@@ -0,0 +1,13 @@
+SnekBall = ("SnekBall.png")
+
+XPos = XPos + XDirection
+YPos = YPos + YDirection
+
+if XPos == 1230:
+    XDirection = -1
+if YPos == 670:
+    YDirection = 
+
+screen.blit(SnekBall, XPos, YPos)
+
+
---
+++ Main.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+XPos = 640
+YPos = 360
+XDirection = 1
+YDirection = 1
+screen = pygame.display.set_mode((1280, 720))
+
+running = True
+while running:
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            running = False
+        if event.type == KEYDOWN:
+            exec("BallMovement.py")
